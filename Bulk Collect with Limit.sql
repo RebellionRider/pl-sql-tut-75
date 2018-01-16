d78febb76f0a605ff5396fb38e5aad8b3b74d4f4
Manish Sharma <32567588+RebellionRider@users.noreply.github.com>
2018-01-16 19:57:11 +0530

    PL/SQL Tut 75

---
+++ Bulk Collect with Limit.sql
@@ -0,0 +1,18 @@
+SET SERVEROUTPUT ON;
+DECLARE
+    CURSOR exp_cur IS
+    SELECT first_name FROM employees;
+    
+    TYPE nt_fName   IS TABLE OF VARCHAR2(20);
+    fname   nt_fName;
+BEGIN
+    OPEN exp_cur;
+    FETCH exp_cur   BULK COLLECT INTO fname     LIMIT 10;
+    CLOSE exp_cur;
+    --Print data
+    FOR idx IN 1 .. fname.COUNT
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(idx||' '||fname(idx) );
+    END LOOP;
+END;
+/
